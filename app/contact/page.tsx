9b39c41b8861fbad178d921c88742d0b5c519a80
Charles Henry Tinoy <charleshenrytinoy8@gmail.com>
2026-04-01 01:32:24 +0800

    Smaller hero section width at Services and Contact Page #28

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -46,12 +46,14 @@ export default function ContactPage() {
 
       {/* Hero section */}
       <section id="hero" aria-labelledby="hero-heading" className="max-w-7xl mx-4 md:mx-6 lg:mx-8 xl:mx-auto py-4 sm:py-8 md:py-12 lg:py-16">
-        <div className="space-y-6">
-          <p className="text-tertiary uppercase font-bold tracking-widest text-xs text-center brightness-50">Get in touch</p>
-          <h1 id="hero-heading" itemProp="name" className="text-black text-center text-2xl sm:text-4xl md:text-5xl font-extrabold font-manrope">Always Here For <span className="text-primary">You.</span></h1>
-          <p itemProp="description" className="text-center">
-            Whether you have questions about our specialized nursing care or need immediate assistance, our team ready to provide the compassionate support your family deserves.
-          </p>
+        <div className="w-full flex justify-center items-center">
+          <div className="space-y-6 max-w-lg">
+            <p className="text-tertiary uppercase font-bold tracking-widest text-xs text-center brightness-50">Get in touch</p>
+            <h1 id="hero-heading" itemProp="name" className="text-black text-center text-2xl sm:text-4xl md:text-5xl font-extrabold font-manrope">Always Here For <span className="text-primary">You.</span></h1>
+            <p itemProp="description" className="text-center">
+              Whether you have questions about our specialized nursing care or need immediate assistance, our team ready to provide the compassionate support your family deserves.
+            </p>
+          </div>
         </div>
       </section>
 
--- app/services/page.tsx
+++ app/services/page.tsx
@@ -82,13 +82,15 @@ export default function ServicesPage() {
     
       {/* Hero section */}
       <section id="hero" aria-labelledby="hero-heading" itemScope itemType="https://schema.org/Service" className="max-w-7xl mx-4 md:mx-6 lg:mx-8 xl:mx-auto py-4 sm:py-8 md:py-12 lg:py-16">
-        <div className="space-y-6">
-          <p className="text-tertiary uppercase font-bold tracking-widest text-xs text-center brightness-50">Our comprehensive care</p>
-          <h1 id="hero-heading" itemProp="name" className="text-black text-center text-2xl sm:text-4xl md:text-5xl font-extrabold font-manrope">Dedicated Support For Your Loved Ones</h1>
-          <p itemProp="description" className="text-center">
-            We provide professional, compassionate home care services tailored to meet the unique needs of every client.
-            {" "}<q itemProp="slogan" className="text-primary">Inspired to care the majestic way</q>
-          </p>
+        <div className="flex w-full items-center justify-center">
+          <div className="space-y-6 max-w-lg">
+            <p className="text-tertiary uppercase font-bold tracking-widest text-xs text-center brightness-50">Our comprehensive care</p>
+            <h1 id="hero-heading" itemProp="name" className="text-black text-center text-2xl sm:text-4xl md:text-5xl font-extrabold font-manrope">Dedicated Support For Your Loved Ones</h1>
+            <p itemProp="description" className="text-center">
+              We provide professional, compassionate home care services tailored to meet the unique needs of every client.
+              {" "}<q itemProp="slogan" className="text-primary">Inspired to care the majestic way</q>
+            </p>
+          </div>
         </div>
       </section>
 
