8091e48a3b4a083bf720629d364a415b26c15a6d
Charles Henry Tinoy <charleshenrytinoy8@gmail.com>
2026-04-02 03:28:57 +0800

    "Leave a review section" at Reviews Page #29

--- app/reviews/page.tsx
+++ app/reviews/page.tsx
@@ -1,3 +1,4 @@
+import Button from "@/components/ui/Button"
 import { BsStarFill } from "react-icons/bs"
 import { LuUser } from "react-icons/lu"
 
@@ -19,8 +20,15 @@ const reviews = [
     rating: 5,
     date: new Date("March 31, 2022"),
     comment: "This agency helped a lot with my nana. The care they provided was excellent and the staff were friendly and accommodating. Especially Maria was very helpful assisting me on the phone with my inquires."
-  },
-]
+  }
+];
+
+const dateFormatOptions: Intl.DateTimeFormatOptions = {
+  weekday: "long",
+  year: "numeric",
+  month: "long",
+  day: "numeric"
+};
 
 export default function ReviewsPage() {
   return (
@@ -42,10 +50,10 @@ export default function ReviewsPage() {
       {/* Reviews section */}
       <section id="reviews" className="my-12 py-12" aria-label="Customer Reviews">
         <div className="max-w-7xl mx-4 md:mx-6 lg:mx-8 xl:mx-auto">
-          <div className="flex flex-row justify-center gap-6">
+          <div className="flex flex-col md:flex-row justify-center gap-6">
 
             {reviews.map((review, index) => (
-              <article key={`review-item-${index}`} className="w-md bg-white rounded-2xl shadow-2xl p-4 sm:p-8 md:p-12" itemScope itemType="https://schema.org/Review">
+              <article key={`review-item-${index}`} className={`w-full md:w-md bg-white rounded-2xl shadow-2xl p-8 md:p-12`} itemScope itemType="https://schema.org/Review">
                 <div className="flex flex-col gap-6 h-full">
 
                   <div className="inline-flex gap-2" itemProp="reviewRating" itemScope itemType="https://schema.org/Rating">
@@ -60,14 +68,36 @@ export default function ReviewsPage() {
 
                   <p itemProp="reviewBody">{review.comment}</p>
 
-                  <div className="flex items-center gap-6 mt-auto" itemProp="author" itemScope itemType="https://schema.org/Person">
-                    <LuUser className="size-10" />
-                    <h2 className="font-manrope text-black font-bold" itemProp="name">{review.user}</h2>
+                  <div className="flex flex-row md:flex-col lg:flex-row items-center gap-6 mt-auto" itemProp="author" itemScope itemType="https://schema.org/Person">
+                    <div className="bg-primary/10 rounded-full p-4">
+                      <LuUser className="size-8" />
+                    </div>
+                    <div className="space-y-1">
+                      <h2 className="font-manrope text-black font-bold" itemProp="name">{review.user}</h2>
+                      <p className="opacity-50 text-sm">{review.date.toLocaleDateString("en-US", dateFormatOptions)}</p>
+                    </div>
                   </div>
 
                 </div>
               </article>
             ))}
+
+          </div>
+        </div>
+      </section>
+
+      {/* Leave a review section */}
+      <section id="final-cta" aria-labelledby="final-cta-heading" className="my-12 py-12" itemScope itemType="https://schema.org/ReviewAction">
+        <div className="max-w-7xl mx-4 md:mx-6 lg:mx-8 xl:mx-auto">
+          <div className="flex justify-center items-center">
+
+            <div className="w-6xl bg-primary p-8 sm:p-12 rounded-3xl text-gray-200 shadow-2xl">
+              <div className="flex flex-col gap-8 items-center justify-center">
+                <h2 id="final-cta-heading" className="text-center text-white font-manrope font-bold text-2xl md:text-4xl">Have We Cared For Your Loved One?</h2>
+                <p className="text-center">Your feedback helps us continue to provide the highest level of care. We would be honored if you shared your experience with us.</p>
+                <button aria-label="Open review form to share your care experience" aria-haspopup="dialog" className="transition bg-white hover:bg-gray-200 active:bg-gray-300 text-primary font-semibold px-8 py-4 rounded-full shadow-2xl text-sm sm:text-md">Leave a Review</button>
+              </div>
+            </div>
 
           </div>
         </div>
