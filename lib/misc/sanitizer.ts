f0818d0514993ac509c26c8332797092ba1d8d13
Charles Henry Tinoy <charleshenrytinoy8@gmail.com>
2026-04-13 00:55:09 +0800

    Moved sanitizer to "misc" dir

---
+++ lib/misc/sanitizer.ts
@@ -0,0 +1,9 @@
+export default function sanitize(str: string): string {
+  if (!str) return "";
+
+  return str
+    .replace(/\0/g, "")
+    .replace(/[^\S\r\n]{2,}/g, " ")
+    .replace(/(?:\r?\n){3,}/g, "\n\n")
+    .trim();
+}
